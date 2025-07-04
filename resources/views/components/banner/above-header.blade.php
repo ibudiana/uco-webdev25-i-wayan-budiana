96c1d7cb28e2f09642847388496cccc736ac01f8
I Wayan Budiana <ibudiana@student.ciputra.ac.id>
2025-07-04 15:53:15 +0800

    update cookie

--- resources/views/components/banner/above-header.blade.php
+++ resources/views/components/banner/above-header.blade.php
@@ -1,4 +1,4 @@
-<div x-data="{ showBanner: true }" x-show="showBanner" x-transition
+<div x-data="{ showBanner: getCookie('promoBannerDismissed') !== 'true'  }" x-show="showBanner" x-transition x-cloak
      class="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-900 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
 
     <!-- Background Gradient Left -->
@@ -31,7 +31,7 @@
 
     <!-- Close Button -->
     <div class="flex flex-1 justify-end">
-        <button @click="showBanner = false" type="button" class="-m-3 p-3 focus-visible:-outline-offset-4">
+        <button @click="setCookie('promoBannerDismissed', 'true', 1); showBanner = false" type="button" class="-m-3 p-3 focus-visible:-outline-offset-4">
             <span class="sr-only">Dismiss</span>
             <svg class="size-5 text-gray-50" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                 <path d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z" />
